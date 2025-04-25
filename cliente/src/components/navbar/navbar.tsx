b4bb47c6544f8298321dab71111eff4ef3b864d9
kochp1 <juanandreskochp@gmail.com>
2025-04-25 18:40:11 -0400

    Add new images and implement navbar component
    - Added various game-related images to the assets directory.
    - Created a new navbar component with styling.
    - Updated index.css to simplify body styling.
    - Introduced a logo SVG for the navbar.

--- cliente/src/components/navbar/navbar.tsx
+++ cliente/src/components/navbar/navbar.tsx
@@ -1,2 +1,19 @@
 import './navbar.css'
 
+
+export const NavBar = () => {
+    return (
+        <nav className='navBar'>
+            <ul className='navList'>
+                <li className="navItem">
+                    <a href="#" className="navLink">
+                        <img src="../../assets/images/logo.svg" alt="Pixelated Paradise" className='imgLogo'/>
+                    </a>
+                </li>
+                <li className="navItem"><a href="#" className="navLink">Nintendo</a></li>
+                <li className="navItem"><a href="#" className="navLink">Sega</a></li>
+                <li className="navItem"><a href="#" className="navLink">Sony</a></li>
+            </ul>
+        </nav>
+    )
+}
