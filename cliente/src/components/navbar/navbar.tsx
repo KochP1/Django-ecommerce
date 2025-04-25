214a6ae4ecd88287923899c7f9e8443d267955ec
kochp1 <juanandreskochp@gmail.com>
2025-04-25 19:57:04 -0400

    feat(navbar): update styles and add options menu
    - Increased gap between navbar items for better spacing.
    - Adjusted font sizes for nav links and added new options links.
    - Updated logo dimensions for improved visibility.
    - Added a dropdown menu for user options including login, settings, and logout.
    - Refactored CSS to reset margin and padding for nav lists.
    - Added global box-sizing and body styles for consistency.
    - Introduced new logo SVG file for branding.

--- cliente/src/components/navbar/navbar.tsx
+++ cliente/src/components/navbar/navbar.tsx
@@ -7,13 +7,36 @@ export const NavBar = () => {
             <ul className='navList'>
                 <li className="navItem">
                     <a href="#" className="navLink">
-                        <img src="../../assets/images/logo.svg" alt="Pixelated Paradise" className='imgLogo'/>
+                        <img src="/logo.svg" alt="Pixelated Paradise" className='imgLogo'/>
                     </a>
                 </li>
                 <li className="navItem"><a href="#" className="navLink">Nintendo</a></li>
                 <li className="navItem"><a href="#" className="navLink">Sega</a></li>
                 <li className="navItem"><a href="#" className="navLink">Sony</a></li>
             </ul>
+
+            <div className='navOptions__container'>
+                <ul className='options-navList navList'>
+                    <li className="options-navLink navItem">
+                        <i className='fa-solid fa-moon'></i>
+                    </li>
+                    <li className="options-navLink navItem">
+                        <i className='fa-solid fa-cart-shopping'></i>
+                    </li>
+                    <li className="options-navLink navItem dropdown">
+                        <i className='fa-solid fa-user'></i>
+
+                        <ul className='dropdown-menu'>
+                            <li><a className="dropdown-item" id="log-in">Log in</a></li>
+                            <li><a className="dropdown-item" href="#" id="user-settings">Settings</a></li>
+                            <li className="dropdown-divider__nav-item" id="drop-divider">
+                                <hr className="dropdown-divider"></hr>
+                            </li>
+                            <li><a className="dropdown-item" id="log-out">Log out</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
